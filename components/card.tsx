2803cc78d413a3ba96f1a36298cd62656dcc217f
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-09-16 21:13:28 -0400

    Changes layout

--- components/card.tsx
+++ components/card.tsx
@@ -1,24 +1,14 @@
 import { useState } from "react";
-
-export interface ImageDetails {
-  copyright: string;
-  date: string;
-  explanation: string;
-  hdurl: string;
-  media_type: string;
-  service_version: string;
-  title: string;
-  url: string;
-}
+import { ImageDetails } from "./Interfaces";
 
 export default function Card({ item }: { item: ImageDetails }) {
   const [lines, setLines] = useState(true);
   const [like, setLike] = useState(false);
   return (
-    <div>
+    <div className="bg-gray-200 mb-4">
       <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md lg:h-80 lg:aspect-none">
         <img
-          src={item.hdurl}
+          src={item.url}
           alt={item.title}
           className="w-full h-full object-center object-cover lg:w-full lg:h-full"
         />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,12 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Card, { ImageDetails } from "../components/card";
+import Card from "../components/card";
 import { useEffect, useState } from "react";
-import styles from "../styles/Home.module.css";
+import { ImageDetails } from "../components/Interfaces";
 
 function ImageList({ data }: { data: ImageDetails[] }) {
   return (
-    <div className="bg-white ">
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {data && data.map((item) => <Card key={item.title} item={item} />)}
-        </div>
-      </div>
+    <div className="mx-auto max-w-md py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+      {data && data.map((item) => <Card key={item.title} item={item} />)}
     </div>
   );
 }
