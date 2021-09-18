b24ad6af315ad98602a0b82c04b1e692524f7dd0
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-09-17 20:27:40 -0400

    Adds safelist classes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    safelist: ["line-clamp-1", "line-clamp-2", "line-clamp-3", "line-clamp-4", "line-clamp-5", "line-clamp-6"]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
