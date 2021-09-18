2a5a3edb26f4bfa63ef212c21375e45d148297d4
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-09-18 12:06:34 -0400

    remove Liked page

--- pages/Liked.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export default function Liked({ liked }: { liked: string[] }) {
-  const [images, setImages] = useState<{}[]>([]);
-
-  for (let i = 0; i < liked.length; i++) {}
-}
