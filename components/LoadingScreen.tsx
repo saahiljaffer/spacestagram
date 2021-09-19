509aeb42d73e530f61808a00a5f0a3b554e056ad
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-09-19 15:10:54 -0400

    Changes loading screen

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -5,8 +5,8 @@ export default function LoadingScreen({ visible }: { visible: boolean }) {
         visible ? "visible" : "invisible"
       }`}
     >
-      {visible}
-      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+      {visible ? "visible" : "invisible"}
+      {/* <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div> */}
     </div>
   );
 }
