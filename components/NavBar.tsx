6f3df11655f6d650197c17351dfdf4f738bf3bfd
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-09-19 11:46:53 -0400

    Changes navbar to be blue and sticky

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
   return (
-    <nav className="mx-auto px-8 sm:px-10 lg:px-12 sticky flex items-center  h-16">
+    <nav className="bg-gray-700 text-white mx-auto px-8 sm:px-10 lg:px-12 sticky top-0 flex items-center h-16">
       spacestagram
     </nav>
   );
